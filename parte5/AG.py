3fe9879d9acd0df707843bc12106d05eabe8ed80
João Marcos <joaomarcosjoao92@gmail.com>
2025-11-27 22:21:09 -0300

    refatorações no código

--- parte5/AG.py
+++ parte5/AG.py
@@ -2,13 +2,12 @@ import numpy as np
 from random import choices
 
 INTERVALO = [-10, 10]
-POPULACAO_INICIAL = 4
+POPULACAO = 4
 TAM_CROMOSSOMO = 5
 
 
 def func(x: int):
     return pow(x, 2) - 3 * x + 4
-
 
 def gerar_indiviuo():
     individuo_int = np.random.randint(low=INTERVALO[0], high=INTERVALO[1])
@@ -24,11 +23,18 @@ def gerar_indiviuo():
     return individuo
 
 
+def converter_para_int(num_binario: str):
+    sinal = -1 if num_binario[0] == 1 else 1
+    num_int = sinal * int(num_binario[0:], 2)
+    return num_int
+
 def calc_prob(cromossomos):
-    soma = np.sum(func(cromossomos))
+    individuos = []
+    individuos.append(converter_para_int(cromossomo) for cromossomo in cromossomos) 
+    soma = np.sum(func(individuos))
     probabilidades = []
 
-    for i in cromossomos:
+    for i in individuos:
         probabilidade = round((func(i) / soma), 2)
         probabilidades.append(probabilidade)
 
@@ -39,27 +45,24 @@ def calc_prob(cromossomos):
     return np.array(probabilidades)
 
 
-def converter_para_int(num_binario: str):
-    sinal = -1 if num_binario[0] == 1 else 1
-    num_int = sinal * int(num_binario[0:], 2)
-    return num_int
-
-
 def gerar_populacao_inicial():
     cromossomos = []
 
-    for _ in range(POPULACAO_INICIAL):
+    for _ in range(POPULACAO):
         cromossomos.append(gerar_indiviuo())
 
     return np.array(cromossomos)
 
 
-def roleta(cromossomos):
+def torneio(cromossomos):
     probabilidades = calc_prob(cromossomos)
+    
+    for _ in range(POPULACAO):
+        escolhas = np.random.choice(a=cromossomos, size=2, p=probabilidades)
 
-    return np.random.choice(a=cromossomos, size=4, p=probabilidades)
+    return 
 
+if __name__ == "__main__":
+    cromossomos = gerar_populacao_inicial()
 
-cromossomos = gerar_populacao_inicial()
-
-print(cromossomos)
+    print(cromossomos)
